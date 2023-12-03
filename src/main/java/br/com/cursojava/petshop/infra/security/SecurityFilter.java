8c25206a97fdd88dc61367505e050326e613b51c
luisp <luispires.m@gmail.com>
2023-12-03 15:44:22 -0300

    api com autenticação

--- src/main/java/br/com/cursojava/petshop/infra/security/SecurityFilter.java
+++ src/main/java/br/com/cursojava/petshop/infra/security/SecurityFilter.java
@@ -1,6 +1,9 @@
 package br.com.cursojava.petshop.infra.security;
 
+import br.com.cursojava.petshop.domain.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,22 +18,29 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
         var tokenJWT = recuperarToken(request);
 
-        var subject = tokenService.getSubject(tokenJWT);
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = userRepository.findByLogin(subject);
+            var authenticarion = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authenticarion);
+        }
         filterChain.doFilter(request,response);
     }
-
     private String recuperarToken(HttpServletRequest request) {
-
         var authorizationHeader = request.getHeader("Authorization");
-        System.out.println("autorization");
-        if(authorizationHeader == null){
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ","");
+        return null;
     }
 }
